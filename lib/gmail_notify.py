1666b30c4d72c85a08ac7e896212373803001858
jackiejay888 <jaciejay888@gmail.com>
2021-10-02 13:31:56 +0800

    Commit the gamil_notify.py and line_notify.py

---
+++ lib/gmail_notify.py
@@ -0,0 +1,39 @@
+#/usr/bin/python3
+'''
+Created on 2021/10/02
+
+@author: ZL Chen
+@title: Sent the email notify.
+'''
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+
+class gmail_notify(object):
+		
+	def gmail(self, sent_email, receive_email):  
+		self.content = MIMEMultipart()  #建立 MIMEMultipart 物件
+		self.content['subject'] = 'Warning!'  #郵件標題
+		self.content['from'] = sent_email  #寄件者
+		self.content['to'] = receive_email #收件者
+		self.content.attach(MIMEText('Dear 5G SVT members,\n\n\tWarning! \
+			\n\tThe email is tell our members.\n\tWe are under a attack! \
+			\n\tPlease make sure the 5G lab environment, thanks. \
+			\n\t"The email is notify by ZL demo.\"\n\nZL.'))  #郵件內容
+
+	def smtplib_smtp(self):
+		with smtplib.SMTP(host='smtp.gmail.com', port='587') as smtp:  # 設定 SMTP 伺服器
+			try:
+				smtp.ehlo()  # 驗證 SMTP 伺服器
+				smtp.starttls()  # 建立加密傳輸
+				smtp.login(self.content['from'], 'aipyrivzffkgwmbs')  # 登入寄件者gmail
+				smtp.send_message(self.content)  # 寄送郵件
+				print('Complete!')
+			except Exception as e:
+				print('Error message: ', e)
+	
+if __name__ == "__main__":
+	g_n = gmail_notify()
+	g_n.gmail('iec100535@gmail.com', 'Chen.ZL@inventec.com,cute71252@gmail.com')
+	g_n.smtplib_smtp()
---
+++ lib/line_notify.py
@@ -0,0 +1,29 @@
+#/usr/bin/python3
+'''
+Created on 2021/10/02
+
+@author: ZL Chen
+@title: Sent the line notify.
+'''
+
+import requests
+
+class line_notify(object):
+		
+	def send_message(self, message):
+		headers = {
+			'Authorization': 'Bearer ' + 'OwasfjGmiKRT8OOQzUUkVy4EmsXvApqI2ZtumsH5jdn', # Sweet Temp
+			'Content-Type': 'application/x-www-form-urlencoded'
+		}
+		params = {'message': message}
+		requests.post('https://notify-api.line.me/api/notify', headers=headers, params=params)
+		# r = requests.post('https://notify-api.line.me/api/notify', headers=headers, params=params)
+		# print('Send Line Message ', r.status_code)  #200
+		
+if __name__ == '__main__':
+	l_n = line_notify()
+	message = '\n\nDear members,\n\n\tWarning! \
+			\n\tWe are under a attack! \
+			\n\tPlease make sure the lab environment, thanks. \
+			\n\t"The LINE NOTIFY is notify by ZL demo.\"\n\nZL.'
+	l_n.send_message(message)
